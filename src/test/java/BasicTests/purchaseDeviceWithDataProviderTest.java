5a660526d382728ded217a44b6d60d8121e332a9
Ntebo Petlele <Ntebo.Petlele@eoh.com>
2026-03-06 02:00:22 +0200

    Added Map for radio buttons selection

--- src/test/java/BasicTests/purchaseDeviceWithDataProviderTest.java
+++ src/test/java/BasicTests/purchaseDeviceWithDataProviderTest.java
@@ -22,22 +22,15 @@ public class purchaseDeviceWithDataProviderTest {
     }
 
     @Test(dataProvider = "purchaseData")
-    public void purchaseDevice(String email,
-                               String password,
-                               String devType,
-                               String devBrand,
-                               String devStorage,
-                               String devColor,
-                               String devQuantity,
-                               String address,
-                               String shipMethod,
-                               String warrantyOption,
-                               String discCode) {
+    public void purchaseDevice(String email, String password, String devType,String devBrand, String devStorage,
+                               String devColor, String devQuantity, String address, String shipMethod,
+                               String warrantyOption, String discCode) {
 
-        // The implementation of the test will go here, using the provided data to fill out the purchase
-        // form and complete the transaction.
+    // The implementation of the test will go here, using the provided data to fill out the purchase form and
+    // complete the transaction.
 
         loginPage.login(email, password);
+
         purchaseDevicePage.fillPurchaseDeviceForm(devType, devBrand, devStorage, devColor, devQuantity,
                 address, shipMethod, warrantyOption, discCode);
 
@@ -46,7 +39,7 @@ public class purchaseDeviceWithDataProviderTest {
     @DataProvider(name = "purchaseData")
     public Object[][] getData() {
         return new Object[][]{
-                {"lizbee@gmail.com", "@123abcde", "Phone", "Apple", "128GB", "Blue", "2", "123 Main Street, Anytown, SA",
+                {"lizbee@gmail.com", "@123abcde", "Phone", "Apple", "128GB", "Blue", "2", "123 Test Street",
                         "express", "1yr", "SAVE10"},
 
         };
--- src/test/java/pages/PurchaseDevicePage.java
+++ src/test/java/pages/PurchaseDevicePage.java
@@ -3,17 +3,20 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PurchaseDevicePage {
 
     WebDriver driver;
     WebDriverWait wait;
+    Map<String, By> deviceStorage = new HashMap<>();
+    Map<String, By> shippingMethod = new HashMap<>();
+    Map<String, By> deviceWarranty = new HashMap<>();
+
 
     By learnBtnNav = By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/button/span[2]");
     By learningMaterial = By.xpath("//*[@id=\"app-root\"]/nav/div[1]/div[2]/div[1]/div/button[2]/span[2]");
@@ -21,14 +24,11 @@ public class PurchaseDevicePage {
     By inventoryForm = By.id("inventory-title");
     By deviceType = By.id("deviceType");
     By deviceBrand = By.id("brand");
-    By deviceStorage = By.id("storage-128GB");
     By deviceColor = By.id("color");
     By deviceQuantity = By.id("quantity");
     By deliveryAddress = By.id("address");
     By nextBtn = By.id("inventory-next-btn");
     By invoicePage = By.id("invoices-toggle-btn");
-    By shippingMethod = By.id("shipping-express");
-    By deviceWarranty = By.id("warranty-1yr");
     By discountCode = By.id("discount-code");
     By applyBtn = By.id("apply-discount-btn");
     By discountFeedback = By.id("discount-feedback");
@@ -36,14 +36,37 @@ public class PurchaseDevicePage {
     By successToast = By.id("purchase-success-toast");
     By viewInvoiceBtn = By.id("view-history-btn");
     By InvoiceHistory = By.xpath("//*[@id=\"invoice-history-title\"]");
-    //By viewInvoiceHistoryBtn = By.id("view-invoice-INV-1772566733012");
     By viewInvoiceHistoryBtn = (By.cssSelector("button[id^='view-invoice-INV']"));
 
     public PurchaseDevicePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        PageFactory.initElements(driver, this);
 
+        deviceStorage.put("64GB", By.id("storage-64GB"));
+        deviceStorage.put("128GB", By.id("storage-128GB"));
+        deviceStorage.put("256GB", By.id("storage-256GB"));
+
+        shippingMethod.put("standard", By.id("shipping-standard"));
+        shippingMethod.put("express", By.id("shipping-express"));
+
+        // By deviceWarranty = By.id("warranty-1yr");
+        deviceWarranty.put("None", By.id("warranty-none"));
+        deviceWarranty.put("1yr", By.id("warranty-1yr"));
+        deviceWarranty.put("2yr", By.id("warranty-2yr"));
+
+    }
+
+    public void selectStorageSize(String devStorage) {
+       driver.findElement(deviceStorage.get(devStorage)).click();
+    }
+
+    public void selectShippingMethod(String shipMethod) {
+        driver.findElement(shippingMethod.get(shipMethod)).click();
+        //driver.findElement(shippingMethod.get(shipMethod.toUpperCase())).click();
+    }
+
+    public void selectDeviceWarranty(String warrantyOption) {
+        driver.findElement(deviceWarranty.get(warrantyOption)).click();
     }
 
     public void fillPurchaseDeviceForm(String devType,
@@ -59,6 +82,8 @@ public class PurchaseDevicePage {
         // Navigate to the learning material and select the web automation advance course
 
         driver.findElement(learnBtnNav).click();
+        WebDriverWait wait7 = new WebDriverWait(driver, java.time.Duration.ofSeconds(10));
+        wait7.until(driver -> driver.findElement(learningMaterial).isDisplayed());
         driver.findElement(learningMaterial).click();
         driver.findElement(webAutomationAdvanceBtn).click();
         WebDriverWait wait2 = new WebDriverWait(driver, java.time.Duration.ofSeconds(20));
@@ -70,7 +95,8 @@ public class PurchaseDevicePage {
         driver.findElement(deviceType).sendKeys(devType);
         driver.findElement(deviceBrand).click();
         driver.findElement(deviceBrand).sendKeys(devBrand);
-        driver.findElement(deviceStorage).click();
+        selectStorageSize(devStorage);
+        //driver.findElement(deviceStorage).click();
         driver.findElement(deviceColor).click();
         driver.findElement(deviceColor).click();
         driver.findElement(deviceColor).sendKeys(devColor);
@@ -81,10 +107,10 @@ public class PurchaseDevicePage {
         driver.findElement(nextBtn).click();
 
         // Order preview and  purchase confirmation
-        WebDriverWait wait3 = new WebDriverWait(driver, java.time.Duration.ofSeconds(5));
+        WebDriverWait wait3 = new WebDriverWait(driver, java.time.Duration.ofSeconds(15));
         wait3.until(driver -> driver.findElement(invoicePage).isDisplayed());
-        driver.findElement(shippingMethod).click();
-        driver.findElement(deviceWarranty).click();
+        selectShippingMethod(shipMethod);
+        selectDeviceWarranty(warrantyOption);
         driver.findElement(discountCode).sendKeys(discCode);
         driver.findElement(applyBtn).click();
         WebDriverWait wait4 = new WebDriverWait(driver, java.time.Duration.ofSeconds(5));
